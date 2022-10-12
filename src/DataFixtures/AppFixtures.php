d25397bfc554b71289a0decfa3bab052fdddbdc6
Damien L <damien.laneelle33@gmail.com>
2022-10-12 14:29:19 +0200

    add fixtures recipes

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ingredient;
+use App\Entity\Recette;
+use DateTimeImmutable;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Faker\Factory;
@@ -27,6 +29,17 @@ class AppFixtures extends Fixture
             $ingredient->setName($this->faker->word())
                 ->setPrice(mt_rand(0, 100));
             $manager->persist($ingredient);
+        }
+
+        for ($i = 0; $i < 50; $i++){
+            $recette = new Recette();
+            $recette->setName($this->faker->sentence(min:3, max:5))
+            ->setTime(mt_rand(1, 24))
+            ->setServe(mt_rand(1, 50))
+            ->setHardness(mt_rand(1, 5))
+            ->setDescription($this->faker->sentences)
+            ->setPrice(mt_rand(0, 1000));
+            $manager->persist($recette);
         }
 
 
---
+++ src/Entity/Recette.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecetteRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: RecetteRepository::class)]
+class Recette
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    #[Assert\Length(min: 2, max: 50)]
+    #[Assert\NotBlank()]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    #[Assert\Positive]
+    #[Assert\Range(min: 1, max: 1440)]
+    private ?int $time = null;
+
+    #[ORM\Column]
+    #[Assert\LessThan(50)]
+    private ?int $serve = null;
+
+    #[ORM\Column]
+    #[Assert\Range(min: 1, max: 5)]
+    private ?int $hardness = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank()]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    #[Assert\Positive]
+    #[Assert\Range(min: 0, max: 1000)]
+    private ?float $price = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getTime(): ?int
+    {
+        return $this->time;
+    }
+
+    public function setTime(int $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getServe(): ?int
+    {
+        return $this->serve;
+    }
+
+    public function setServe(int $serve): self
+    {
+        $this->serve = $serve;
+
+        return $this;
+    }
+
+    public function getHardness(): ?int
+    {
+        return $this->hardness;
+    }
+
+    public function setHardness(int $hardness): self
+    {
+        $this->hardness = $hardness;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+   
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
